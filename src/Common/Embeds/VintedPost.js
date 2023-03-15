33785b72afccc85e35434fbb45b9b17b2220f9c8
powerm1nt <nukaworks@nuka.works>
2023-03-15 21:55:55 +0100

    :construction: Make pagination system

--- src/Common/Embeds/VintedPost.js
+++ src/Common/Embeds/VintedPost.js
@@ -1,33 +1,60 @@
-const { EmbedBuilder } = require('discord.js')
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, Events } = require('discord.js')
 
-async function makePost (interaction, id, title, description, price, image, link) {
+async function makePost (interaction, posts) {
+  const postIndex = 0
+  const lastPostIndex = posts.length - 1
   const avatar = interaction.client.user.avatarURL()
+  const buttons = new ActionRowBuilder()
+    .addComponents(
+      new ButtonBuilder()
+        .setCustomId('prevVintedPost')
+        .setLabel('Prev')
+        .setStyle('Secondary'),
+
+      new ButtonBuilder()
+        .setCustomId('nextVintedPost')
+        .setLabel('Next')
+        .setStyle('Primary')
+    )
+
+  console.log(posts[0])
+
+  const embed = new EmbedBuilder()
+    .setColor(0x0099FF)
+    .setTitle('title')
+    .setURL('https://discord.js.org/')
+    .setAuthor({
+      name: 'Some name',
+      iconURL: 'https://i.imgur.com/AfFp7pu.png',
+      url: 'https://discord.js.org'
+    })
+    .setDescription('description')
+  // .setThumbnail('image')
+  // .setImage('https://i.imgur.com/AfFp7pu.png')
+    .setTimestamp()
+    .setFooter({ text: interaction.client.user.tag, iconURL: avatar })
+
+  interaction.client.on(Events.InteractionCreate, interaction => {
+    if (!interaction.isButton()) return
+    if (interaction.customId === 'nextVintedPost') {
+      interaction.update({
+        components: [buttons],
+        embeds: [embed]
+      })
+    }
+
+    if (interaction.customId === 'prevVintedPost') {
+      interaction.update({
+        components: [buttons],
+        embeds: [embed]
+      })
+    }
+  })
 
   return await interaction.reply({
-    embeds: [
-      new EmbedBuilder()
-        .setColor(0x0099FF)
-        .setTitle('Some title')
-        .setURL('https://discord.js.org/')
-        .setAuthor({
-          name: 'Some name',
-          iconURL: 'https://i.imgur.com/AfFp7pu.png',
-          url: 'https://discord.js.org'
-        })
-        .setDescription('Some description here')
-        .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-        .addFields(
-          { name: 'Regular field title', value: 'Some value here' },
-          { name: '\u200B', value: '\u200B' },
-          { name: 'Inline field title', value: 'Some value here', inline: true },
-          { name: 'Inline field title', value: 'Some value here', inline: true }
-        )
-        .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-        .setImage('https://i.imgur.com/AfFp7pu.png')
-        .setTimestamp()
-        .setFooter({ text: interaction.client.user.tag, iconURL: avatar })
-    ]
+    components: [buttons],
+    embeds: [embed]
   })
 }
 
-module.exports = { createPost: makePost }
+module.exports = { createPostCollection: makePost }
