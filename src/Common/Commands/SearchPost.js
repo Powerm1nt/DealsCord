05e1598e74e3f2b21e85499834e3f672a4bb9344
powerm1nt <nukaworks@nuka.works>
2023-03-15 21:56:43 +0100

    :construction: Impl makePost

--- src/Common/Commands/SearchPost.js
+++ src/Common/Commands/SearchPost.js
@@ -1,14 +1,27 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js')
 const makePost = require('../Embeds/VintedPost')
-const { createPost } = require('../Embeds/VintedPost')
+const { createPostCollection } = require('../Embeds/VintedPost')
+const vinted = require('@powerm1nt/vinted-api')
 
 const searchCommand = new SlashCommandBuilder()
   .setName('search')
   .setDescription('Search for a vinted post')
-
+  .addStringOption(option =>
+    option
+      .setName('keywords')
+      .setDescription('Keywords to search for')
+      .setRequired(true)
+  )
 module.exports = {
   data: searchCommand,
   async execute (interaction) {
-    await createPost(interaction)
+    await vinted.fetchCookie()
+      .then(async (data) => {
+        await vinted.search(`https://www.vinted.fr/vetements?search_text=${interaction.options.getString('keywords')}?page=1`).then(async (data) => {
+          const vintedPosts = data.items
+
+          await createPostCollection(interaction, vintedPosts)
+        })
+      })
   }
 }
