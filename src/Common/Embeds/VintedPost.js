279045942038fd86f67906b43683c38cfa99f69a
powerm1nt <nukaworks@nuka.works>
2023-03-16 20:10:31 +0100

    :sparkles: Pagination is now stable

--- src/Common/Embeds/VintedPost.js
+++ src/Common/Embeds/VintedPost.js
@@ -1,6 +1,6 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, Events } = require('discord.js')
 
-function generateEmbed (posts, interaction, postIndex) {
+function generateEmbed (posts, interaction, postIndex, lastPostIndex) {
   const avatar = interaction.client.user.avatarURL()
 
   const embed = new EmbedBuilder()
@@ -12,20 +12,37 @@ function generateEmbed (posts, interaction, postIndex) {
       url: posts[postIndex].user.url
     })
     .setThumbnail(posts[postIndex].user.photo.url)
-    .setImage(posts[postIndex].photo.url)
+    .setImage(posts[postIndex].photo.full_size_url)
     .addFields(
       { name: 'Price', value: Math.round(posts[postIndex].price * 100) / 100 + '€', inline: false },
       { name: 'Reputation', value: posts[postIndex].favourite_count.toString(), inline: false }
     )
     .setTimestamp()
     .setFooter({ text: interaction.client.user.tag, iconURL: avatar })
-  posts[postIndex].size_title >= 1 && embed.addField({
+  posts[postIndex].size_title >= 1 && embed.addFields({
     name: 'Size',
     value: posts[postIndex].size_title,
     inline: false
   })
 
-  return embed
+  const buttons = new ActionRowBuilder()
+    .addComponents(
+      new ButtonBuilder()
+        .setCustomId('prevVintedPost')
+        .setLabel('Prev')
+        .setStyle('Secondary')
+        .setDisabled(postIndex === 0),
+
+      new ButtonBuilder()
+        .setCustomId('nextVintedPost')
+        .setLabel('Next')
+        .setStyle('Primary')
+        .setDisabled(postIndex === lastPostIndex)
+    )
+
+  console.log(posts[0])
+
+  return { components: [buttons], embeds: [embed] }
 }
 
 class VintedPost {
@@ -35,45 +52,20 @@ class VintedPost {
     this.postIndex = 0
     this.lastPostIndex = this.posts.length - 1
 
-    const buttons = new ActionRowBuilder()
-      .addComponents(
-        new ButtonBuilder()
-          .setCustomId('prevVintedPost')
-          .setLabel('Prev')
-          .setStyle('Secondary'),
-
-        new ButtonBuilder()
-          .setCustomId('nextVintedPost')
-          .setLabel('Next')
-          .setStyle('Primary')
-      )
-
-    console.log(posts[0])
-
     interaction.client.on(Events.InteractionCreate, interaction => {
       if (!interaction.isButton()) return
       if (interaction.customId === 'nextVintedPost') {
         this.postIndex++
-        console.log(this.postIndex)
-        interaction.update({
-          components: [buttons],
-          embeds: [generateEmbed(posts, interaction, this.postIndex)]
-        })
+        interaction.update(generateEmbed(posts, interaction, this.postIndex, this.lastPostIndex))
       }
 
       if (interaction.customId === 'prevVintedPost') {
         this.postIndex--
-        interaction.update({
-          components: [buttons],
-          embeds: [generateEmbed(posts, interaction, this.postIndex)]
-        })
+        interaction.update(generateEmbed(posts, interaction, this.postIndex, this.lastPostIndex))
       }
     })
 
-    return await interaction.reply({
-      components: [buttons],
-      embeds: [generateEmbed(posts, interaction, this.postIndex)]
-    })
+    return await interaction.reply(generateEmbed(posts, interaction, this.postIndex, this.lastPostIndex))
   }
 }
 
