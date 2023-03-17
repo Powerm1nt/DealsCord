6d5a838555d0882ddbd43b583cefdbac21766ba9
powerm1nt <nukaworks@nuka.works>
2023-03-17 21:14:00 +0100

    :sparkles: Added optional search arguments

--- src/Common/Commands/SearchPost.js
+++ src/Common/Commands/SearchPost.js
@@ -11,11 +11,32 @@ const searchCommand = new SlashCommandBuilder()
       .setDescription('Keywords to search for')
       .setRequired(true)
   )
+  .addStringOption(option =>
+    option
+      .setName('price')
+      .setDescription('Price to search for')
+      .setRequired(false)
+  )
+  .addStringOption(option =>
+    option
+      .setName('size')
+      .setDescription('Size to search for')
+      .setRequired(false)
+  )
+  .addStringOption(option =>
+    option
+      .setName('reputation')
+      .setDescription('Reputation to search for')
+      .setRequired(false)
+  )
+
 module.exports = {
   data: searchCommand,
   async execute (interaction) {
     await vinted.fetchCookie()
       .then(async (data) => {
+        const searchUrl = `https://www.vinted.fr/vetements?search_text=${interaction.options.getString('keywords')}&price_from=${interaction.options.getString('price')}&size=${interaction.options.getString('size')}&reputation=${interaction.options.getString('reputation')}`
+
         await vinted.search(`https://www.vinted.fr/vetements?search_text=${interaction.options.getString('keywords')}?page=1`).then(async (data) => {
           await new VintedPost().makePost(interaction, data.items)
         })
