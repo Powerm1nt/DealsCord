4fa748193a0450c06ab97d0dcbd4075845d3a2dc
powerm1nt <nukaworks@nuka.works>
2023-03-19 18:40:01 +0100

    :recycle: Rewrite model to a mongoose model

--- src/Common/DataModels/Alert.js
+++ src/Common/DataModels/Alert.js
@@ -1,25 +1,21 @@
-const { Model, Sequelize } = require('sequelize')
+const { Schema } = require('mongoose')
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'data.db'
-})
-
-class Alert extends Model {
-}
-
-Alert.init({
+const Alert = new Schema({
   name: 'string',
   keywords: 'string',
   guildId: 'string',
   channelId: 'string',
-  author: 'number',
+  author: 'string',
   interval: 'number',
   price: 'number',
   size: 'string',
   reputation: 'number',
   page: 'number',
-  filter: 'string'
-}, { sequelize, modelName: 'alert' })
+  filter: 'string',
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
 
 module.exports = Alert
