e9f6b4047fe17385c9b3e667cc18139623fe46fc
powerm1nt <nukaworks@nuka.works>
2023-03-19 18:39:28 +0100

    :art: Code Reformat

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-   env: {
-      commonjs: true,
-      browser: true,
-      es2021: false
-   },
-   extends: [
-      'standard'
-   ],
-   parserOptions: {
-      ecmaFeatures: {
-         jsx: true
-      },
-      ecmaVersion: 'latest',
-      sourceType: 'module'
-   },
-   rules: {
-      camelcase: 'off',
-      'no-unused-vars': 'off'
-   }
+  env: {
+    commonjs: true,
+    browser: true,
+    es2021: false
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    camelcase: 'off',
+    'no-unused-vars': 'off'
+  }
 }
