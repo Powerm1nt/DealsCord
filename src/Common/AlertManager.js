8934b4fd071440156e091c7a04f9d3bd6c89a7c2
powerm1nt <nukaworks@nuka.works>
2023-03-19 17:46:48 +0100

    :construction: Debugging the db

--- src/Common/AlertManager.js
+++ src/Common/AlertManager.js
@@ -2,8 +2,7 @@ const Alert = require('./DataModels/Alert')
 const parse = require('parse-duration')
 const vinted = require('@powerm1nt/vinted-api')
 const { generateEmbed } = require('./Embeds/VintedCollections')
-const { getClient } = require('../index')
-const { ToadScheduler, SimpleIntervalJob, Task, AsyncTask } = require('toad-scheduler')
+const { ToadScheduler, SimpleIntervalJob, AsyncTask } = require('toad-scheduler')
 
 class AlertManager {
   scheduler = new ToadScheduler()
@@ -12,59 +11,73 @@ class AlertManager {
     this.alerts = []
   }
 
+  async syncAlerts (interaction) {
+    return await Alert.findAll()
+      .then(async (data) => {
+        console.log(data)
+        for (const alert of data) {
+          console.log('Channel NAME : ' + alert.channelId)
+          const searchUrl = `https://www.vinted.fr/vetements?search_text=${alert.keywords}&price_from=${alert.price}&size=${alert.size}&reputation=${alert.reputation}&order=${alert.filter}&page=${alert.page}`
+
+          await vinted.fetchCookie().then(async (data) => {
+            await vinted.search(searchUrl).then(async (data) => {
+              alert.cache = data.items
+            })
+          })
+
+          const task = new AsyncTask(`alert_${alert.name}`, async () => {
+            console.log('Checking for new posts...')
+            await vinted.fetchCookie()
+              .then(async (data) => {
+                await vinted.search(searchUrl).then(async (data) => {
+                  console.log('CHANNEL ID : ' + alert.channelId)
+                  interaction.client.channels.fetch(alert.channelId).then(async (channel) => {
+                    for (const item of data.items) {
+                      //  Check if the post is already in the cache
+                      if (!alert.cache.find(c => c.id === item.id)) {
+                        //  If not, send the embed and add it to the cache
+                        await channel.send({
+                          content: '✨ **Nouveau Post trouvé !**',
+                          embeds: [generateEmbed(item, null)]
+                        })
+
+                        alert.cache.push(item)
+                      }
+                    }
+                  })
+                })
+              })
+          }, (err) => {
+            console.log(err)
+          })
+
+          this.alerts.push({
+            name: alert.name,
+            data: alert
+          })
+
+          console.log(this.alerts)
+
+          this.scheduler.addSimpleIntervalJob(new SimpleIntervalJob({ seconds: alert.interval }, task))
+        }
+      })
+  }
+
   async addAlert (alert, interaction) {
+    console.log(alert)
     const _date = parse(alert.interval.toString(), 'second')
     if (!_date || _date < 60) throw new Error('Interval must be at least 1 minute')
     alert.interval = _date
-    console.log(alert)
-    const searchUrl = `https://www.vinted.fr/vetements?search_text=${alert.keywords}&price_from=${alert.price}&size=${alert.size}&reputation=${alert.reputation}&order=${alert.filter}&page=${alert.page}`
     // check if the alert already exists on the database
-    const exists = await Alert.findOne({ where: { name: alert.name } })
-    if (exists || this.alerts.find(f => f.name === alert.name)) throw new Error('Alert already exists')
-
-    const item = Alert.build(alert)
-    await Alert.sync()
-    await vinted.fetchCookie().then(async (data) => {
-      await vinted.search(searchUrl).then(async (data) => {
-        alert.cache = data.items
-      })
+    await Alert.findOne({ where: { name: alert.name } }).then(async (data) => {
+      if (data) throw new Error('Alert already exists')
     })
 
-    return await item.save().then(async () => {
-      const task = new AsyncTask(`alert_${alert.name}`, async () => {
-        console.log('Checking for new posts...')
-        await vinted.fetchCookie()
-          .then(async (data) => {
-            await vinted.search(searchUrl).then(async (data) => {
-              interaction.client.channels.fetch(alert.channel).then(async (channel) => {
-                for (const item of data.items) {
-                  //  Check if the post is already in the cache
-                  if (!alert.cache.find(c => c.id === item.id)) {
-                    alert.cache.push(item)
-
-                    //  If not, send the embed and add it to the cache
-                    await channel.send({
-                      content: '✨ **Nouveau Post trouvé !**',
-                      embeds: [generateEmbed(item, null)]
-                    })
-                  }
-                }
-              })
-            })
-          })
-      }, (err) => {
-        console.log(err)
-      })
-
-      this.scheduler.addSimpleIntervalJob(new SimpleIntervalJob({ seconds: alert.interval }, task))
-      this.alerts.push({
-        name: alert.name,
-        data: alert,
-        cache: []
-      })
-
-      return alert
+    await Alert.create(alert).then(async () => {
+      await this.syncAlerts(interaction)
     })
+
+    return alert
   }
 
   async removeAlert (name) {
