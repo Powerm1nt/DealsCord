9d95bebaf80792ed617e7f75e37c613363df2b81
powerm1nt <nukaworks@nuka.works>
2023-03-20 21:19:42 +0100

    :sparkles: Added ListAlert

---
+++ src/Common/Commands/ListAlert.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('discord.js')
+const { getAlertManager } = require('../AlertManager')
+
+const listCommand = new SlashCommandBuilder()
+  .setName('list')
+  .setDescription('List all alerts')
+
+module.exports = {
+  data: listCommand,
+  async execute (interaction) {
+    return await getAlertManager().getAlerts().then(alerts => {
+      // Display a list of alerts
+      const alertList = alerts.map(alert => {
+        console.log(alert)
+        return {
+          name: alert.name,
+          value: `Keywords: **${alert.data.keywords}** | Interval: **${alert.data.interval} seconds**`
+        }
+      })
+
+      const embed = {
+        title: 'Alerts',
+        description: '**List of all alerts**',
+        fields: alertList || [{ name: 'No alerts', value: 'No alerts found' }]
+      }
+
+      interaction.reply({ embeds: [embed] })
+    })
+  }
+}
