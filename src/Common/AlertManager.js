764308414a95203c2d369d83d869c33885389560
Leonard Chabot <nukaworks@nuka.works>
2023-06-03 17:00:34 +0200

    :bug: Using standalone for to iterate data

--- src/Common/AlertManager.js
+++ src/Common/AlertManager.js
@@ -77,10 +77,9 @@ class AlertManager {
           // Fetch the cookie and search for the posts
           vinted.fetchCookie().then((data) => {
             vinted.search(searchUrl).then((data) => {
-              console.log(data)
-              data.forEach(({ items }) => {
-                alert.cache.push(items)
-              })
+              for (const item of data.items) {
+                alert.cache.push(item)
+              }
             })
           })
 
