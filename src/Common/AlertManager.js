869f3985a04dc535f08bc43f333ebd8125edc2a7
Leonard Chabot <nukaworks@nuka.works>
2023-06-03 17:05:06 +0200

    :bug: Trying Array.from(data.items)

--- src/Common/AlertManager.js
+++ src/Common/AlertManager.js
@@ -77,7 +77,7 @@ class AlertManager {
           // Fetch the cookie and search for the posts
           vinted.fetchCookie().then((data) => {
             vinted.search(searchUrl).then((data) => {
-              for (const item of data.items) {
+              for (const item of Array.from(data.items)) {
                 alert.cache.push(item)
               }
             })
