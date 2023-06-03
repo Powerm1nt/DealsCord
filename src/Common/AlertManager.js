fe410810272047360594d71ee2e4b0d78ad69106
Leonard Chabot <nukaworks@nuka.works>
2023-06-03 13:59:20 +0200

    :bug: DEBUG

--- src/Common/AlertManager.js
+++ src/Common/AlertManager.js
@@ -93,7 +93,7 @@ class AlertManager {
                       for (const item of data.items) {
                         //  Check if the post is already in the cache
                         if (!alert.cache.find(c => c.id === item.id)) {
-                          console.log('Alert found ' + alert.id)
+                          console.log('Alert found ' + item.id)
                           // Check if the post is in the excluded types
                           if (excludeCategory(item.url, excluded_types || 'maison', 'accessoires', 'divertissement')) {
                             console.log('Excluding post ' + item.id + ' ' + item.title)
