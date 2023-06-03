3d7b97ff0feb99ca117d9e416337383e16b4145a
Leonard Chabot <nukaworks@nuka.works>
2023-06-03 15:48:25 +0200

    :bug: DEBUG typeof alert.cache

--- src/Common/AlertManager.js
+++ src/Common/AlertManager.js
@@ -108,7 +108,6 @@ class AlertManager {
                           })
 
                           // Add it to cache
-                          console.log(alert.cache)
                           alert.cache.push(item)
                         }
                       }
